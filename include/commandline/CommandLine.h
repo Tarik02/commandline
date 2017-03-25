c467dd40b9c4cba431a241330fbb899b3b74a03b
Tarik02 <Taras.Fomin@gmail.com>
2017-03-25 21:18:43 +0200

    Initial commit.

---
+++ include/commandline/CommandLine.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace commandline {
+	class CommandLine {
+	private:
+		std::vector <std::string> argv;
+		std::vector<std::string>::iterator it;
+
+	public:
+		CommandLine(const std::vector <std::string> &argv);
+		CommandLine(int argc, char *argv[]);
+
+		bool param(const std::string &longName, char shortName = '\0');
+		bool next(std::string &param);
+	};
+}
---
+++ src/commandline/CommandLine.cpp
@@ -0,0 +1,51 @@
+#include "commandline/CommandLine.h"
+
+namespace commandline {
+	CommandLine::CommandLine(const std::vector <std::string> &argv) :
+		argv(argv) {
+		it = this->argv.begin() + 1;
+	}
+
+	CommandLine::CommandLine(int argc, char *argv[]) :
+		argv() {
+		this->argv.reserve((unsigned long)argc);
+
+		do {
+			this->argv.emplace_back(*argv);
+			++argv;
+		} while (--argc);
+
+		it = this->argv.begin() + 1;
+	}
+
+	bool CommandLine::param(const std::string &longName, char shortName = '\0') {
+		if (it == argv.end()) {
+			return false;
+		}
+
+		const auto &arg = *it;
+
+		if (shortName != '\0') {
+			if ((arg.size() == 2) && (arg[1] == shortName)) {
+				++it;
+				return true;
+			}
+		}
+
+		if ((arg.size() > 2) && (arg.substr(2) == longName)) {
+			++it;
+			return true;
+		}
+
+		return false;
+	}
+
+	bool CommandLine::next(std::string &param) {
+		if (it == argv.end()) {
+			return false;
+		}
+
+		param = *it;
+		return true;
+	}
+}
